3570a7e8f0eb024b3f6c58f5a4e2fa6012e8dc81
Proje969 <projehesap07@gmail.com>
2025-01-15 00:30:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,36 +9,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static('public'));
 
-// Routes
+// Serve static files
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'about.html'));
-});
-
-app.get('/services', (req, res) => {
-    res.sendFile(path.join(__dirname, 'services.html'));
-});
-
-app.get('/roadmap', (req, res) => {
-    res.sendFile(path.join(__dirname, 'roadmap.html'));
-});
-
-// Chat API endpoint
-app.post('/api/chat', (req, res) => {
-    const { message } = req.body;
-    
-    // Basic response for now
-    // In the future, this will integrate with a real AI service
-    setTimeout(() => {
-        res.json({
-            message: "Hello! I'm Zethara AI. I'm still learning and will be able to help you better soon!"
-        });
-    }, 1000);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start server
